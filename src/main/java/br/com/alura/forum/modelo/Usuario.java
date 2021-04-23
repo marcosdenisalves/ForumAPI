83c2dc6690ea2066e13b018f5afe3c4fe09ee3fb
Study Profile <dee.alves2@gmail.com>
2021-04-23 20:58:10 -0300

    Usando Spring data

--- src/main/java/br/com/alura/forum/modelo/Usuario.java
+++ src/main/java/br/com/alura/forum/modelo/Usuario.java
@@ -1,7 +1,17 @@
 package br.com.alura.forum.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
--- src/main/java/br/com/alura/forum/modelo/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/modelo/controller/TopicoController.java
@@ -1,21 +1,29 @@
 package br.com.alura.forum.modelo.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.modelo.dtos.TopicoDTO;
+import br.com.alura.forum.repositories.TopicoRepository;
 
 @RestController
 public class TopicoController {
 
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
-	public List<TopicoDTO> lista() {
-		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-		return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+	public List<TopicoDTO> lista(String nomeCurso) {
+		if(nomeCurso == null) {
+			List<Topico> list = topicoRepository.findAll();
+			return TopicoDTO.converter(list);
+		} else {
+			List<Topico> list = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDTO.converter(list);
+		}
 	}
 }
