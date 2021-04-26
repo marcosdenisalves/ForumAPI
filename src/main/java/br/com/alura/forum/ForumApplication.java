4b86b12a6a19d60d30bbc9c39811935bfc4f16b5
Study Profile <dee.alves2@gmail.com>
2021-04-26 19:04:41 -0300

    Paginação e ordenação de recursos

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport //Modulo habilitado para permitir que o spring forneça os parametros atravez da url 
 public class ForumApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/alura/forum/modelo/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/modelo/controller/TopicoController.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.modelo.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -38,12 +42,15 @@ public class TopicoController {
 	private CursoRepository cursoRepository;
 	
 	@GetMapping
-	public List<TopicoDTO> lista(String nomeCurso) {
+	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, 
+			@PageableDefault(sort= "id", direction = Direction.DESC,
+			page = 0, size = 5)  Pageable paginacao) {
+
 		if(nomeCurso == null) {
-			List<Topico> list = topicoRepository.findAll();
+			Page<Topico> list = topicoRepository.findAll(paginacao);
 			return TopicoDTO.converter(list);
 		} else {
-			List<Topico> list = topicoRepository.findByCursoNome(nomeCurso);
+			Page<Topico> list = topicoRepository.findByCursoNome(nomeCurso, paginacao);
 			return TopicoDTO.converter(list);
 		}
 	}
