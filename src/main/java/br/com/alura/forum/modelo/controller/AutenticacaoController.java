9c68e4a610299749bcc4769bb936b1ed6234d0d5
Study Profile <dee.alves2@gmail.com>
2021-04-26 22:15:04 -0300

    Gerando Token com JWT

--- src/main/java/br/com/alura/forum/modelo/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/modelo/controller/AutenticacaoController.java
@@ -2,20 +2,42 @@ package br.com.alura.forum.modelo.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.modelo.dto.LoginDTO;
+import br.com.alura.forum.modelo.dto.TokenDto;
 
 @RestController
 @RequestMapping("/auth")
 public class AutenticacaoController {
 
+	@Autowired
+	private AuthenticationManager authManager;
+	//Por padrão essa classe não vem configurada pra injeção de dependencias
+	
+	@Autowired
+	private TokenService tokenService;
+	
 	@PostMapping
-	public ResponseEntity<?> autenticar(@RequestBody @Valid LoginDTO loginDto) {
-		return ResponseEntity.ok().build();	
+	public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginDTO loginDto) {
+		UsernamePasswordAuthenticationToken dadosLogin =  loginDto.converter();
+
+		try {
+			Authentication authentication = authManager.authenticate(dadosLogin); //Spring verifica as configurações criadas e chama a classe AutenticacaoService
+			String token = tokenService.gerarToken(authentication);
+			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+		} catch (AuthenticationException e) {
+			return ResponseEntity.badRequest().build();
+		}
 	}
 }
--- src/main/java/br/com/alura/forum/modelo/dto/LoginDTO.java
+++ src/main/java/br/com/alura/forum/modelo/dto/LoginDTO.java
@@ -1,5 +1,7 @@
 package br.com.alura.forum.modelo.dto;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 public class LoginDTO {
 
 	private String email;
@@ -19,5 +21,9 @@ public class LoginDTO {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public UsernamePasswordAuthenticationToken converter() {
+		return new UsernamePasswordAuthenticationToken(email, senha);
 	}
 }
